00c6a709bef981f7d6070b55a46f96a313f63e2a
marho42 <87908028+marho42@users.noreply.github.com>
2025-01-25 08:14:59 -0500

    load.lua

---
+++ load.lua
@@ -0,0 +1,13 @@
+local AkaliNotif = loadstring(game:HttpGet("https://raw.githubusercontent.com/Kinlei/Dynissimo/main/Scripts/AkaliNotif.lua"))();
+local Notify = AkaliNotif.Notify;
+
+wait(1);
+
+Notify({
+Description = "Executed";
+Title = "MiteHub | V1";
+Duration = 3;
+});
+
+wait(1);
+
